597f70201b6641235847d08cf15ec2a777490d10
Alex Funk <lexotron314@gmail.com>
2016-09-28 12:55:04 -0500

    Removed docker, added vp9 support, cleaned temp files

--- encode.sh
+++ encode.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ "$1" == "-h" ] || [ "$1" == "--help" ] || [ $# -eq 0 ]; then
+  echo "Usage: $0 <source filename> [--vp9]"
+  echo "<source filename> = video to be transcoded"
+  echo "--vp9 = live in the future, and also make a vp9/opus container"
+  echo ""
+  exit 1
+fi
+
 # Get the directory and filename
 DIR=$(dirname "$1")
 FILE=$(basename "$1")
@@ -7,9 +15,6 @@ FILE=$(basename "$1")
 # Go into the directory so the container will work locally
 cd $DIR
 
-# Temporarily install the actual encode script
-# Gets "mounted into" the container with FFMPEG and tackles all the real encoding
-cat <<'EOF' > encode-inner.sh
 IN=$1
 OUT=$(echo $1 | sed 's/^\(.*\)\.[a-zA-Z0-9]*$/\1/')
 
@@ -32,12 +37,6 @@ else
     echo "--- Rotation detected; changed scale param"
 fi
 
-# Sometimes you need to force this if, for example, your video is sideways but doesn't contain that meta data
-if [ "$FORCE" == "1" ]; then
-    echo "!!! Forced scale + rotation"
-    VF="scale=-1:720,transpose=2"
-fi
-
 # Count cores, more than one? Use many!
 # Uses one less than total (recomendation for webm)
 # Doesn't apply to x264 where 0 == auto (webm doesn't support that)
@@ -48,7 +47,7 @@ fi
 
 echo "--- Using $CORES threads for webm"
 
-echo "--- webm, First Pass"
+echo "--- vp8 webm, First Pass"
 ffmpeg -i $IN \
     -hide_banner -loglevel error -stats \
     -codec:v libvpx -threads $CORES -slices 4 -quality good -cpu-used 0 -b:v 1000k -qmin 10 -qmax 42 -maxrate 1000k -bufsize 2000k -vf $VF \
@@ -57,16 +56,16 @@ ffmpeg -i $IN \
     -f webm \
     -y /dev/null
 
-echo "--- webm, Second Pass"
+echo "--- vp8 webm, Second Pass"
 ffmpeg -i $IN \
     -hide_banner -loglevel error -stats \
     -codec:v libvpx -threads $CORES -slices 4 -quality good -cpu-used 0 -b:v 1000k -qmin 10 -qmax 42 -maxrate 1000k -bufsize 2000k -vf $VF \
     -codec:a libvorbis -b:a 128k \
     -pass 2 \
     -f webm \
-    -y $OUT.webm
+    -y $OUT-vp8.webm
 
-echo "--- x264, First Pass"
+echo "--- x264 mp4, First Pass"
 ffmpeg -i $IN \
     -hide_banner -loglevel error -stats \
     -codec:v libx264 -threads 0 -profile:v main -preset slow -b:v 1000k -maxrate 1000k -bufsize 2000k -vf $VF \
@@ -75,25 +74,34 @@ ffmpeg -i $IN \
     -f mp4 \
     -y /dev/null
 
-echo "--- x264, Second Pass"
+echo "--- x264 mp4, Second Pass"
 ffmpeg -i $IN \
     -hide_banner -loglevel error -stats \
     -codec:v libx264 -threads 0 -profile:v main -preset slow -b:v 1000k -maxrate 1000k -bufsize 2000k -vf $VF \
-    -codec:a libfdk_aac -b:a 128k \
+    -codec:a aac -b:a 128k -strict -2\
     -pass 2 \
     -f mp4 \
-    -y $OUT.mp4
-EOF
+    -y $OUT-h264.mp4
 
-# Run the container. Note that we set the workingdir to /tmp since there are a few cruft files
-# the two stage encoding produces that we don't want to leave around in `pwd`
-docker run -t --rm \
-  -e "FORCE=$FORCE"\
-  -v `pwd`:/app \
-  -w /tmp \
-  --entrypoint='bash' \
-  jrottenberg/ffmpeg \
-  /app/encode-inner.sh /app/${FILE}
+if [ "$2" == "--vp9" ]; then
 
-# Remove that temp script
-rm -f encode-inner.sh
+    echo "--- vp9 webm, First Pass"
+    ffmpeg -i $IN \
+        -hide_banner -loglevel error -stats \
+        -codec:v libvpx-vp9 -threads $CORES -speed 4 -b:v 700k -tile-columns 6 -frame-parallel 1 -vf $VF \
+        -an \
+        -pass 1 \
+        -f webm \
+        -y /dev/null
+
+    echo "--- vp9 webm, Second Pass"
+    ffmpeg -i $IN \
+        -hide_banner -loglevel error -stats \
+        -codec:v libvpx-vp9 -threads $CORES -speed 1 -b:v 700k -tile-columns 6 -frame-parallel 1 -auto-alt-ref 1 -lag-in-frames 25 -vf $VF \
+        -codec:a libopus -b:a 64k \
+        -pass 2 \
+        -f webm \
+        -y $OUT-vp9.webm
+fi
+
+rm -f ffmpeg2pass*
